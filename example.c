c5d531295f18dd7e3f664af300e62e3663e781a7
David Rauch <mail@davidrauch.at>
2014-07-17 19:03:37 +0200

    Initial Commit

---
+++ example.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "germanc.h"
+
+ganzzahl main(ganzzahl artc, zeichen** argv) {
+	klein ganzzahl i = 0;
+
+	während(i < 10) {
+		printf("Hallo Welt\n");
+		i++;
+	}
+	
+	rückgabe 0;
+}
---
+++ germanc.h
@@ -0,0 +1,1 @@
+#include "germanc_keywords.h"
